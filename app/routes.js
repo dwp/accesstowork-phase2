54b424cbcb36b18cd79072e00443cf4527ff133f
Ash-Wilson <ashleighwilson.digitaldwp@gmail.com>
2017-02-27 14:55:00 +0000

    re-routing for new prototype

--- app/routes.js
+++ app/routes.js
@@ -3,46 +3,46 @@ var router = express.Router()
 
 // Route index page
 router.get('/', function (req, res) {
-  res.render('start_page')
+  res.render('index')
 })
 
 // add your routes here
 
 /// EMPLOYED ROUTES ///
-router.post('/employment_type', function (req, res) {
+router.post('/Payments_login_desktop_v1/employment_type', function (req, res) {
         if (req.body.claimtype === 'employed') {
-          res.redirect('/aboutyou_employed')
+          res.redirect('/Payments_login_desktop_v1/aboutyou_employed')
         } else {
-          res.redirect('/aboutyou_selfemployed')
+          res.redirect('/Payments_login_desktop_v1/aboutyou_selfemployed')
         }
       });
 
-router.post('/claim_type', function (req, res) {
+router.post('/Payments_login_desktop_v1/claim_type', function (req, res) {
           if (req.body.claimtype === 'Travel') {
-            res.redirect('/travel_type')
+            res.redirect('/Payments_login_desktop_v1/travel_type')
           } else if (req.body.claimtype ==='Support') {
-            res.redirect('/support_details')
+            res.redirect('/Payments_login_desktop_v1/support_details')
           } else {
-            res.redirect('/one_off_claim')
+            res.redirect('/Payments_login_desktop_v1/one_off_claim')
           }
         });
 
-router.post('/travel_type', function (req, res) {
+router.post('/Payments_login_desktop_v1/travel_type', function (req, res) {
         if (req.body.claim === 'travel1') {
-          res.redirect('/single_travel')
+          res.redirect('/Payments_login_desktop_v1/single_travel')
         } else {
-          res.redirect('/multi_travel')
+          res.redirect('/Payments_login_desktop_v1/multi_travel')
         }
       });
 
 /// SELF EMPLOYED ROUTES ///
-router.post('/self_claim_type', function (req, res) {
+router.post('/Payments_login_desktop_v1/self_claim_type', function (req, res) {
           if (req.body.claimtype === 'Travel') {
-            res.redirect('/travel_type')
+            res.redirect('/Payments_login_desktop_v1/travel_type')
           } else if (req.body.claimtype ==='Support') {
-            res.redirect('/support_details')
+            res.redirect('/Payments_login_desktop_v1/support_details')
           } else {
-            res.redirect('/self_oneoff')
+            res.redirect('/Payments_login_desktop_v1/self_oneoff')
           }
         });
 
