e02b0995b69938589e3b37c18b84112e048e2908
Ash-Wilson <ashleighwilson.digitaldwp@gmail.com>
2017-02-27 12:05:46 +0000

    Revert "adding routes and sass files"
    This reverts commit 727ecaed66a754c1bb03815d8d56a7083bc0b4da.

--- app/routes.js
+++ app/routes.js
@@ -3,49 +3,9 @@ var router = express.Router()
 
 // Route index page
 router.get('/', function (req, res) {
-  res.render('start_page')
+  res.render('index')
 })
 
 // add your routes here
-
-/// EMPLOYED ROUTES ///
-router.post('/employment_type', function (req, res) {
-        if (req.body.claimtype === 'employed') {
-          res.redirect('/aboutyou_employed')
-        } else {
-          res.redirect('/aboutyou_selfemployed')
-        }
-      });
-
-router.post('/claim_type', function (req, res) {
-          if (req.body.claimtype === 'Travel') {
-            res.redirect('/travel_type')
-          } else if (req.body.claimtype ==='Support') {
-            res.redirect('/support_details')
-          } else {
-            res.redirect('/one_off_claim')
-          }
-        });
-
-router.post('/travel_type', function (req, res) {
-        if (req.body.claim === 'travel1') {
-          res.redirect('/single_travel')
-        } else {
-          res.redirect('/multi_travel')
-        }
-      });
-
-/// SELF EMPLOYED ROUTES ///
-router.post('/self_claim_type', function (req, res) {
-          if (req.body.claimtype === 'Travel') {
-            res.redirect('/travel_type')
-          } else if (req.body.claimtype ==='Support') {
-            res.redirect('/support_details')
-          } else {
-            res.redirect('/self_oneoff')
-          }
-        });
-
-
 
 module.exports = router
