272af09a26d3ebbe7d5627832600fe968994cf87
Richard Smith <srich.leeds@gmail.com>
2017-02-28 13:54:56 +0000

    Adds routes files for payments journey

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,58 @@ router.get('/', function (req, res) {
 })
 
 
+/// PAYMENTS VERSION-1 ///
+
+/// EMPLOYED ROUTES ///
+router.post('/payments-v1/employment-type', function (req, res) {
+        if (req.body.claimtype === 'employed') {
+          res.redirect('/payments-v1/user-details')
+        } else {
+          res.redirect('/payments-v1/self-user-details')
+        }
+      });
+
+router.post('/payments-v1/claim-type', function (req, res) {
+          if (req.body.claimtype === 'Travel') {
+            res.redirect('/payments-v1/travel-type')
+          } else if (req.body.claimtype ==='Support') {
+            res.redirect('/payments-v1/support-single')
+          } else {
+            res.redirect('/payments-v1/one-off-claim')
+          }
+        });
+
+router.post('/payments-v1/travel-type', function (req, res) {
+        if (req.body.claim === 'travel1') {
+          res.redirect('/payments-v1/single-travel')
+        } else {
+          res.redirect('/payments-v1/multi-travel')
+        }
+      });
+
+/// SELF EMPLOYED ROUTES ///
+router.post('/payments-v1/self-claim-type', function (req, res) {
+          if (req.body.claimtype === 'Travel') {
+            res.redirect('/payments-v1/self-travel-type')
+          } else if (req.body.claimtype ==='Support') {
+            res.redirect('/payments-v1/self-support-single')
+          } else {
+            res.redirect('/payments-v1/self-one-off-claim')
+          }
+        });
+
+router.post('/payments-v1/self-travel-type', function (req, res) {
+        if (req.body.travel === 'travel1') {
+          res.redirect('/payments-v1/self-single-travel')
+        } else {
+          res.redirect('/payments-v1/self-multi-travel')
+        }
+      });
+
+/// PAYMENTS JOURNEY END ///
+
+/// DESKTOP LOGIN ///
+
 /// EMPLOYED ROUTES ///
 router.post('/Payments_login_desktop_v1/employment_type', function (req, res) {
         if (req.body.claimtype === 'employed') {
@@ -44,6 +96,8 @@ router.post('/Payments_login_desktop_v1/self_claim_type', function (req, res) {
             res.redirect('/Payments_login_desktop_v1/self_oneoff')
           }
         });
+
+/// DESKTOP LOGIN END ///
 
 
 
