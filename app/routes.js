923af4afa3f334700dc77b0985dc5f5e0b676e36
Ash-Wilson <ashleighwilson.digitaldwp@gmail.com>
2017-02-28 12:38:54 +0000

    merge conflict
    removed stuff from routes

--- app/routes.js
+++ app/routes.js
@@ -6,13 +6,6 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-// add your routes here
-<<<<<<< HEAD
-=======
-
-/// DESKTOP ROUTES ///
-
->>>>>>> parent of e0c7e77... Merge remote-tracking branch 'origin/master'
 
 /// EMPLOYED ROUTES ///
 router.post('/Payments_login_desktop_v1/employment_type', function (req, res) {
@@ -52,52 +45,6 @@ router.post('/Payments_login_desktop_v1/self_claim_type', function (req, res) {
           }
         });
 
-<<<<<<< HEAD
-=======
-
-
-/// MOBILE ROUTES ///
-
-/// EMPLOYED ROUTES ///
-router.post('/Payments_login_mobile_v1/employment_type', function (req, res) {
-        if (req.body.claimtype === 'employed') {
-          res.redirect('/Payments_login_mobile_v1/aboutyou_employed')
-        } else {
-          res.redirect('/Payments_login_mobile_v1/aboutyou_selfemployed')
-        }
-      });
-
-router.post('/Payments_login_mobile_v1/claim_type', function (req, res) {
-          if (req.body.claimtype === 'Travel') {
-            res.redirect('/Payments_login_mobile_v1/travel_type')
-          } else if (req.body.claimtype ==='Support') {
-            res.redirect('/Payments_login_mobile_v1/support_details')
-          } else {
-            res.redirect('/Payments_login_mobile_v1/one_off_claim')
-          }
-        });
-
-router.post('/Payments_login_mobile_v1/travel_type', function (req, res) {
-        if (req.body.claim === 'travel1') {
-          res.redirect('/Payments_login_mobile_v1/single_travel')
-        } else {
-          res.redirect('/Payments_login_mobile_v1/multi_travel')
-        }
-      });
-
-/// SELF EMPLOYED ROUTES ///
-router.post('/Payments_login_mobile_v1/self_claim_type', function (req, res) {
-          if (req.body.claimtype === 'Travel') {
-            res.redirect('/Payments_login_mobile_v1/travel_type')
-          } else if (req.body.claimtype ==='Support') {
-            res.redirect('/Payments_login_mobile_v1/support_details')
-          } else {
-            res.redirect('/Payments_login_mobile_v1/self_oneoff')
-          }
-        });
-
-
->>>>>>> parent of e0c7e77... Merge remote-tracking branch 'origin/master'
 
 
 module.exports = router
