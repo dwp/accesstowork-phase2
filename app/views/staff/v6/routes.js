9bf13e876654a6f5712af8174984883d86009a83
srich.leeds@gmail.com <srich.leeds@gmail.com>
2018-02-09 14:41:55 +0000

    Setting up v6

---
+++ app/views/staff/v6/routes.js
@@ -0,0 +1,43 @@
+var express     = require('express'),
+    fs          = require("fs"),
+    util        = require("util"),
+    _           = require("lodash"),
+    moment      = require('moment'),
+    tog         = require(appRoot + '/lib/tog.js'),
+    router      = express.Router();
+
+router.all('*',function(req,res,next)
+{
+  res.locals.PATH = 'staff/v5'
+  next();
+});
+
+router.get('/', function(req,res,next)
+{
+  res.redirect('your_cases');
+});
+
+router.get('/citizen/support', function(req,res,next)
+{
+  if (res.locals.data.support == "Yes")
+  {
+    next();
+  } else if  (res.locals.data.support == "No") {
+    res.redirect('support/rejected');
+  } else {
+    res.redirect('support/initial');
+  }
+});
+
+router.post('/citizen/case/eligibility', function(req,res,next)
+{
+  return (req.body.support == 'No') ? 
+    res.redirect('/staff/v5/citizen/support/rejected') : 
+    res.redirect('/staff/v5/citizen/timeline');
+});
+
+router.post('/citizen/support/award-confirm', function(req,res,next) {
+  res.render('staff/v5/citizen/support/award-confirm', {data:req.body});
+});
+
+module.exports = router;
